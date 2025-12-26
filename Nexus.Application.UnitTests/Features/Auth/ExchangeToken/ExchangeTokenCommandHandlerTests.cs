de43626948e3d595171b34a2bbde6e2e26e25ba9
Saxolotl <erin@saxolotl.xyz>
2025-12-26 14:00:50 +0000

    Re-add user cast as it is needed here

--- Nexus.Application.UnitTests/Features/Auth/ExchangeToken/ExchangeTokenCommandHandlerTests.cs
+++ Nexus.Application.UnitTests/Features/Auth/ExchangeToken/ExchangeTokenCommandHandlerTests.cs
@@ -258,7 +258,7 @@ public class ExchangeTokenCommandHandlerTests
 
         _mockUserRepository
             .Setup(s => s.GetByDiscordIdAsync(discordUser.Id, It.IsAny<CancellationToken>()))
-            .ReturnsAsync(null);
+            .ReturnsAsync((User?)null);
 
         _mockUserRepository
             .Setup(s => s.CreateAsync(discordUser.Id, discordUser.Username, It.IsAny<CancellationToken>()))
